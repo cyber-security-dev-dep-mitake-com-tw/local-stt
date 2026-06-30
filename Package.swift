1d7d95568cc9d43e909c4bb8834f0367c4ea5fe5
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-06-30 17:10:14 +0800

    update : xcode pacgage

--- Package.swift
+++ Package.swift
@@ -8,15 +8,9 @@ let package = Package(
         .executable(name: "LocalSTT", targets: ["LocalSTTApp"]),
         .library(name: "LocalSTTCore", targets: ["LocalSTTCore"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-testing.git", exact: "0.12.0")
-    ],
     targets: [
         .target(name: "LocalSTTCore"),
         .executableTarget(name: "LocalSTTApp", dependencies: ["LocalSTTCore"]),
-        .testTarget(name: "LocalSTTCoreTests", dependencies: [
-            "LocalSTTCore",
-            .product(name: "Testing", package: "swift-testing")
-        ])
+        .testTarget(name: "LocalSTTCoreTests", dependencies: ["LocalSTTCore"])
     ]
 )
